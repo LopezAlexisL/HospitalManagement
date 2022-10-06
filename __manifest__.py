42ffac939f7be042256373647f12776382d2ea7d
Alexis Lopez <alexislopez@outlook.com.ar>
2022-10-06 18:11:39 -0300

    add chat, default search and group by, and base appointment menu

--- __manifest__.py
+++ __manifest__.py
@@ -9,13 +9,15 @@
     'license': 'LGPL-3',
     'category': 'Hospital Management',
     'depends': [
-        'base'
+        'base',
+        'mail',
     ],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
         'views/patient_view.xml',
         'views/female_patient.xml',
+        'views/appointment_view.xml'
     ],
     'demo': [
         ''
--- models/patient.py
+++ models/patient.py
@@ -3,11 +3,13 @@ from odoo import api, fields, models
 
 class Patient(models.Model):
     _name = 'hospital.patient'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
 
     name = fields.Char(string='Patient Name')
     last_name = fields.Char(string='Patient Last Name')
     identification_num = fields.Integer(string='ID Number')
-    age = fields.Char(string='Age')
+    age = fields.Integer(string='Age')
     gender = fields.Selection(selection=[('m', 'Male'), ('f', 'Female'), ('o', 'Other')], string='Gender')
     active = fields.Boolean(string='Active', default=True)
     
