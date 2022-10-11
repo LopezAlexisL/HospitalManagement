1148bf3132f5fc8f25acba78704b5768befdc97a
Alexis Lopez <alexislopez@outlook.com.ar>
2022-10-11 18:34:00 -0300

    added default values in appointment, date handling, related fields, usage of onchange and depend and _rec_name

--- models/appointment.py
+++ models/appointment.py
@@ -5,5 +5,15 @@ class Appointment(models.Model):
     _name = 'hospital.appointment'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description='Hospital Appointment'
+    _rec_name = 'patient_id'
 
     patient_id= fields.Many2one(comodel_name='hospital.patient', string='Patient')
+    appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
+    booking_time = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    gender = fields.Selection(related='patient_id.gender')
+    identification_num = fields.Char(string='DNI') #funciona tambien con un related='patient_id.identification_number'
+
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.identification_num = self.patient_id.identification_num
--- models/patient.py
+++ models/patient.py
@@ -1,15 +1,25 @@
+from datetime import date
 from odoo import api, fields, models
 
 
 class Patient(models.Model):
     _name = 'hospital.patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
+    
 
-
-    name = fields.Char(string='Patient Name')
-    last_name = fields.Char(string='Patient Last Name')
+    name = fields.Char(string='Patient Name', tracking=True)
+    last_name = fields.Char(string='Patient Last Name', tracking=True)
     identification_num = fields.Integer(string='ID Number')
-    age = fields.Integer(string='Age')
-    gender = fields.Selection(selection=[('m', 'Male'), ('f', 'Female'), ('o', 'Other')], string='Gender')
+    age = fields.Integer(string='Age', compute='_compute_age', tracking=True)
+    birth_date = fields.Date(string='Birth Date')
+    gender = fields.Selection(selection=[('m', 'Male'), ('f', 'Female'), ('o', 'Other')], string='Gender', default='m')
     active = fields.Boolean(string='Active', default=True)
     
+    @api.depends('birth_date')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.birth_date:
+                rec.age = today.year - rec.birth_date.year - ((today.month, today.day) < (rec.birth_date.month, rec.birth_date.day))
+            else:
+                rec.age = 0
