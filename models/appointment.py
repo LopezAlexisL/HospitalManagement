53d8b3ba737523e1f1c73100cb98403a45f0cf48
Alexis Lopez <alexislopez@outlook.com.ar>
2022-10-12 17:33:46 -0300

    added notebook & pages, html field, statusbar, buttons, several widgets destined to upgrade interface

--- models/appointment.py
+++ models/appointment.py
@@ -11,9 +11,30 @@ class Appointment(models.Model):
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_time = fields.Date(string='Booking Date', default=fields.Date.context_today)
     gender = fields.Selection(related='patient_id.gender')
-    identification_num = fields.Char(string='DNI') #funciona tambien con un related='patient_id.identification_number'
+    identification_num = fields.Char(string='DNI', help='Patient ID Number') #funciona tambien con un related='patient_id.identification_number'
+    prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], string='Priority')
+    state = fields.Selection(selection=[
+        ('draft', 'Draft'),
+        ('in_consultation', 'In consultation'),
+        ('done', 'Done'),
+        ('canceled', 'Canceled')], default='draft', string='Status', required=True)
+    doctor = fields.Many2one(comodel_name='res.users', string='Doctor')
 
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
         self.identification_num = self.patient_id.identification_num
+
+    def action_button(self):
+        return {
+            'effect': {
+                'fadeout': 'slow',
+                'message': 'Done Succesfully',
+                'type': 'rainbow_man'
+            }
+        }
